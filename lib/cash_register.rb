f29e941c53d36dd6dac016850778a952872b179f
Zach <zmommaerts@gmail.com>
2021-12-15 11:56:03 -0600

    stuck

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    attr_accessor :total, :employee_discount
 
+    def initialize(employee_discount = false)
+        @total = 0
+        @employee_discount = employee_discount
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+    end
+
+    def apply_discount()
+end
