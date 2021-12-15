5c99beb8ecac6bd6f18225fd273f73c68ccff9a4
Zach <zmommaerts@gmail.com>
2021-12-15 12:16:40 -0600

    stuck

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-    attr_accessor :total, :discount, :items
+    attr_accessor :total, :discount, :items, :last_transaction
 
     def initialize(discount = 0)
         @total = 0
@@ -8,16 +8,22 @@ class CashRegister
       end
 
     def add_item(title, price, quantity = 1)
+        @last_transaction = price
         self.total += price * quantity
-        self.items << title
+        quantity.times {|item| self.items << title}
     end
 
     def apply_discount()
         if discount > 0 
-            self.total = self.total - (self.total * (discount/100))
+            self.total = self.total - (self.total * (discount/100).to_f)
             "After the discount, the total comes to #{self.total}."
         else
             'There is no discount to apply.'
         end
     end
+
+    def void_last_transaction
+        self.total -= last_transaction
+    end
+
 end
