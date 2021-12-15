69b8577b24bc29ce189ade53a080601081fa3411
Zach <zmommaerts@gmail.com>
2021-12-15 12:19:40 -0600

    commit tree c059dd7ad4cdd03e48200a69578e3d67938f4e27

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,15 +8,16 @@ class CashRegister
       end
 
     def add_item(title, price, quantity = 1)
-        @last_transaction = price
+        @last_transaction = price * quantity
         self.total += price * quantity
         quantity.times {|item| self.items << title}
     end
 
     def apply_discount()
         if discount > 0 
-            self.total = self.total - (self.total * (discount/100).to_f)
-            "After the discount, the total comes to #{self.total}."
+            discount_as_percent = (100.0 - self.discount.to_f) / 100
+            self.total = (self.total * discount_as_percent).to_i
+            "After the discount, the total comes to $#{self.total}."
         else
             'There is no discount to apply.'
         end
